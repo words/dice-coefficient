ec10a4cb460800e18707a07f68fdaeee6a52dc7a
wooorm <tituswormer@gmail.com>
2014-09-21 22:24:31 +0200

    Refactor spec

--- spec/dice-coefficient.spec.js
+++ spec/dice-coefficient.spec.js
@@ -1,16 +1,33 @@
 'use strict';
 
-var diceCoefficient, assert;
+var diceCoefficient,
+    assert;
+
+/**
+ * Module dependencies.
+ */
 
 diceCoefficient = require('..');
 assert = require('assert');
 
-describe('diceCoefficient()', function () {
-    it('should work', function () {
+/**
+ * Unit tests.
+ */
+
+describe('diceCoefficient(value, alternative)', function () {
+    it('should return a distance of `1` for `abc` and `abc`', function () {
         assert(diceCoefficient('abc', 'abc') === 1);
-        assert(diceCoefficient('abc', 'xyz') === 0);
-        assert(diceCoefficient('night', 'nacht') === 0.25);
     });
+
+    it('should return a distance of `0` for `abc` and `xyz`', function () {
+        assert(diceCoefficient('abc', 'xyz') === 0);
+    });
+
+    it('should return a distance of `0.25` for `night` and `nacht`',
+        function () {
+            assert(diceCoefficient('night', 'nacht') === 0.25);
+        }
+    );
 
     it('should match case insensitive', function () {
         assert(diceCoefficient('NIGHT', 'NaChT') === 0.25);
