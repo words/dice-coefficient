2be30504b6148134171d3f591b62aa3669e90b91
wooorm <tituswormer@gmail.com>
2014-10-27 09:54:55 +0100

    Refactor test

--- test.js
+++ test.js
@@ -1,33 +1,31 @@
 'use strict';
 
+/**
+ * Dependencies.
+ */
+
 var diceCoefficient,
     assert;
-
-/**
- * Module dependencies.
- */
 
 diceCoefficient = require('./');
 assert = require('assert');
 
 /**
- * Unit tests.
+ * Tests.
  */
 
 describe('diceCoefficient(value, alternative)', function () {
-    it('should return a distance of `1` for `abc` and `abc`', function () {
+    it('should be `1` for `abc` and `abc`', function () {
         assert(diceCoefficient('abc', 'abc') === 1);
     });
 
-    it('should return a distance of `0` for `abc` and `xyz`', function () {
+    it('should be `0` for `abc` and `xyz`', function () {
         assert(diceCoefficient('abc', 'xyz') === 0);
     });
 
-    it('should return a distance of `0.25` for `night` and `nacht`',
-        function () {
-            assert(diceCoefficient('night', 'nacht') === 0.25);
-        }
-    );
+    it('should be `0.25` for `night` and `nacht`', function () {
+        assert(diceCoefficient('night', 'nacht') === 0.25);
+    });
 
     it('should match case insensitive', function () {
         assert(diceCoefficient('NIGHT', 'NaChT') === 0.25);
