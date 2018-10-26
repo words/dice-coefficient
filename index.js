ff923bc4505543a70e9e81827d354a7b2a402889
Titus <tituswormer@gmail.com>
2018-10-26 11:30:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ module.exports = diceCoefficient;
 
 /* Get the edit-distance according to Dice between two values. */
 function diceCoefficient(value, alternative) {
-  var normalValue = String(value).toLowerCase()
-  var normalAlternative = String(alternative).toLowerCase()
+  var normalValue = String(value).toLowerCase();
+  var normalAlternative = String(alternative).toLowerCase();
   var left = value.length === 1 ? [normalValue] : bigrams(normalValue);
   var right = alternative.length === 1 ? [normalAlternative] : bigrams(normalAlternative);
   var rightLength = right.length;
